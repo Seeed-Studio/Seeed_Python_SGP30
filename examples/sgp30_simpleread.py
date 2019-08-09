8a854985851ad498eca1b24241a2d18cdbfea865
Baozhu Zuo <zuobaozhu@gmail.com>
2019-08-09 15:20:28 +0800

    Update sgp30_simpleread.py

--- examples/sgp30_simpleread.py
+++ examples/sgp30_simpleread.py
@@ -2,7 +2,8 @@ import seeed_sgp30
 from grove.i2c import Bus
 
 sgp30 = seeed_sgp30.grove_sgp30(Bus())
-data = sgp30.read_measurements()
-co2_eq_ppm, tvoc_ppb = data.data
-print("\r  tVOC = {} ppb CO2eq = {}  ".format(
-                             tvoc_ppb, co2_eq_ppm))
+while True:
+  data = sgp30.read_measurements()
+  co2_eq_ppm, tvoc_ppb = data.data
+  print("\r  tVOC = {} ppb CO2eq = {}  ".format(
+                               tvoc_ppb, co2_eq_ppm))
