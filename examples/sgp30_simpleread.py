acc0f6d5de48f9e57a615f036a80e4f85160cc2a
Baozhu Zuo <zuobaozhu@gmail.com>
2019-08-09 15:12:37 +0800

    It must have been a hand error

--- examples/sgp30_simpleread.py
+++ examples/sgp30_simpleread.py
@@ -3,6 +3,6 @@ from grove.i2c import Bus
 
 sgp30 = seeed_sgp30.grove_sgp30(Bus())
 data = sgp30.read_measurements()
-co2_eq_ppm, tvoc_ppb = reading.data
+co2_eq_ppm, tvoc_ppb = data.data
 print("\r  tVOC = {} ppb CO2eq = {} ppm elapse = {:8.2f}S ".format(
                              tvoc_ppb, co2_eq_ppm, elapse))
